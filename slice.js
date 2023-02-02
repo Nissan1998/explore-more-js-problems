b3631db3c1d63d7292c53407a67f6f30d970bff8
Nissan1998 <jnnissanm@gmail.com>
2023-02-03 01:19:09 +0600

    sliceAnArray

---
+++ slice.js
@@ -0,0 +1,5 @@
+const friends = [34,53,53,123,54,54,64];
+const partial = friends.slice(1, 4);
+console.log(partial);
+console.log(friends);
+// if we want to take some partial values from array we have to use .slice() method after the name of array.
