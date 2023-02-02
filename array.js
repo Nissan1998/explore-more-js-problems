830510ed82cd165ce3e52440d8344548715a5eb8
Nissan1998 <jnnissanm@gmail.com>
2023-02-03 01:05:07 +0600

    allVariables

---
+++ array.js
@@ -0,0 +1,15 @@
+const country = 'Bangladesh';
+const age = 25;
+const isIndependent = true;
+const nissan ={
+    color: 'White',
+    height : 5.8,
+    age : 24,
+    name : 'Nissan',
+}
+const numbers = [32,34,53,45];
+console.log(typeof country);
+console.log(typeof age);
+console.log( typeof isIndependent);
+console.log(typeof nissan);
+console.log( Array.isArray(numbers));
