9f800cd3f7d54ffe1d18e78ae73eef25059e3fec
Nissan1998 <jnnissanm@gmail.com>
2023-02-03 17:07:41 +0600

    cheapestPhoneCalculator

---
+++ cheapestphone.js
@@ -0,0 +1,34 @@
+const phones =[
+    {
+        name: 'samsung', camera : 20, storage: 32, price : 30000 ,
+    },
+
+    {
+        name: 'Oppo', camera : 20, storage: 32, price : 20000 ,
+    },
+
+    {
+        name: 'redmi', camera : 20, storage: 32, price : 18000 ,
+    },
+
+    {
+        name: 'Walton', camera : 20, storage: 32, price : 25000 ,
+    },
+
+    {
+        name: 'nokia', camera : 20, storage: 32, price : 10000 ,
+    }
+];
+
+function cheapestPhone( phones){
+    let cheapPhone = phones[0];
+    for( let i =0; i< phones.length; i++){
+        const phone = phones[i];
+        if( phone.price < cheapPhone.price){
+            cheapPhone = phone;
+        }
+    }
+    return cheapPhone;
+}
+const myPhone = cheapestPhone(phones);
+console.log(myPhone);
